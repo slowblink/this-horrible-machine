477a1b7cebad2a5b78bdf8528852da32966d3b46
aarond707 <aarond707@gmail.com>
2023-02-23 13:36:06 -0800

    started main scene tree
    camera3d isn't working as intended. gonna look at some examples.

---
+++ project/Main.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var hud : Control = $HUD
+@onready var menu : Control = $Menu
+@onready var main_3d : Node3D = $Main3D
+@onready var camera : Camera3D = $Main3D/Camera3D
+
+var level_instance : Node3D 
+
+func _ready():
+	pass 
+
+func unload_level():
+	if (is_instance_valid(level_instance)):
+		level_instance.queue_free()
+	level_instance = null
+
+func load_level(level_name : String):
+	unload_level()
+	var level_path := "res://levels/%s.tscn" % level_name
+	var level_resource := load(level_path)
+	if(level_resource):
+		level_instance = level_resource.instantiate()
+		main_3d.add_child(level_instance)
+
+
+func _on_load_test_scene_pressed():
+	load_level("test_scene")
