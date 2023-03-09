38e29efe77b8ea9ba812174ffc2ccb88af2eded4
aarond707 <aarond707@gmail.com>
2023-03-09 14:31:31 -0800

    audio test
    attenuation features seem better than before

---
+++ project/modules/levels/tutorial_island/Machine.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_animation_player_animation_started(anim_name):
+	pass # Replace with function body.
+
+
+func _on_animation_player_animation_finished(anim_name):
+	pass # Replace with function body.
+
+
+func _on_visible_on_screen_notifier_3d_screen_entered():
+	pass # Replace with function body.
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	pass # Replace with function body.
+
+
