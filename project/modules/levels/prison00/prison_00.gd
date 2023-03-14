37a1402c8b6e7da9ca8b97554a6d654695a82c4c
aarond707 <aarond707@gmail.com>
2023-03-14 00:45:03 -0700

    addded some todos

---
+++ project/modules/levels/prison00/prison_00.gd
@@ -0,0 +1,3 @@
+extends Node3D
+
+# TODO make this level exit to the overworld map, and make a link back
---
+++ project/modules/levels/underworld/underworld.gd
@@ -0,0 +1,4 @@
+extends Node3D
+
+# TODO is one big level which spawns smaller levels into itself when the player gets near
+# TODO build the level loading and unloading triggers
